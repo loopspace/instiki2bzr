42353e519bec2650d4ecf58c4cb0df761911878c
Andrew Stacey <astacey@Thargelion>
2010-04-02 23:49:22 +0200

    Database connection seems to work, just need to figure out interface to bzr

--- i2bzr.pl
+++ i2bzr.pl
@@ -1,16 +1,40 @@
 #! /usr/bin/perl -w
 
 use strict;
+use DBI();
+use Apache::Htpasswd;
+use Apache::Htaccess;
+use URI::Escape;
 
-# Read in from config
+my $database = "instiki";
+my $host = "localhost";
+my $user = "instiki";
+my $pass = "SVt6GX1rmqWw";
 
-my $database = $ENV{HOME} . '/current/programs/instiki_svn/db/production.db.sqlite3';
-my $destination = $ENV{HOME} . '/public_html/nforge';
-my $lastID = 0;
+my $destination = '/var/www/nForge';
+my $htpasswd_file = $destination . '/.htpasswd';
 
-#Haven't got the modules.  Darn.  Let's map out the scheme.
+if (!-e $htpasswd_file) {
+    open (HTPASSWD, ">$htpasswd_file");
+    close HTPASSWD;
+}
 
-#Select pages from last time we ran.
+my $config = $ENV{HOME} . '/.i2bzr.cfg';
+my $lastID = 28250;
+if (-e $config) {
+if (open (CONFIG, $config)) {
+    while (<CONFIG>) {
+	if (/^\s*LastRevision\s*[=:]?\s*(\d+)/) {
+	    $lastID = $1;
+	}
+    }
+    close CONFIG;
+}
+}
+
+my $htpasswd = new Apache::Htpasswd($htpasswd_file);
+my @htusers = $htpasswd->fetchUsers();
+my $dbh = DBI->connect("DBI:mysql:database=" . $database . ";host=" . $host, $user, $pass, {'RaiseError' => 1});
 
 #Need to know:
 # 1. content -> file contents
@@ -36,20 +60,31 @@ my @inittables = qw/
 webs
 /;
 
-my $initrows = getRows($database,\@initcols,\@inittables);
+my $initsql = "SELECT " . join(',',@initcols) . ' FROM ' . join(' JOIN ', @inittables) . ";";
 
-foreach my $row (@$initrows)
-{
-    if ($$row{'password'} ne '')
-    {
-	add_to_htpasswd($$row{'name'}, $$row{'password'});
-	add_to_htaccess($$row{'id'}, 'Requre User ' . $$row{'name'});
+my $init = $dbh->prepare($initsql);
+$init->execute();
+
+while (my $row = $init->fetchrow_hashref()) {
+    if (!-e $destination . '/'. $row->{'id'}) {
+	mkdir($destination . '/' . $row->{'id'});
     }
-    print "Would now make " . $destination . '/' . $$row{'id'} . "\n";
-    print "And link it to " . $destination . '/' . $$row{'address'} . "\n";
-#    mkdir($destination . '/' . $$row['id']);
-#    link($destination . '/' . $$row['address']);
+    if (!-e $destination . '/'. $row->{'address'}) {
+	symlink($destination . '/' . $row->{'id'},$destination . '/' . $row->{'address'});
+    }
+
+    if ($row->{'password'} && ($row->{'password'} ne '')) {
+	my $htaccess = new Apache::Htaccess($destination . '/' . $row->{'id'} . '/' . '.htaccess');
+	$htaccess->directives('AuthType' => 'Basic', 'AuthName' => '"Access to ' . $row->{'name'} . '"', 'AuthUserFile' => $htpasswd_file, 'Require user' => $row->{'name'});
+	$htaccess->save();
+	if (grep($_ eq $row->{'name'}, @htusers)) {
+	    $htpasswd->htpasswd($row->{'name'}, $row->{'password'}, {'overwrite' => 1});
+	} else {
+	    $htpasswd->htpasswd($row->{'name'}, $row->{'password'});
+	}
+    }
 }
+
 
 my @datacols = qw/
 revisions.id
@@ -57,7 +92,7 @@ revisions.content
 revisions.updated_at
 revisions.author
 revisions.ip
-pages.id
+revisions.page_id
 pages.name
 pages.web_id
 /;
@@ -67,69 +102,31 @@ pages
 revisions
 /;
 
-my @dataconditions = qq/
-pages.id=revisions.page_id
-revisions.id>$lastID
-/;
+my @dataconditions = ("pages.id=revisions.page_id","revisions.id>$lastID");
 
-my $datarows = getRows($database,\@datacols,\@datatables,\@dataconditions);
+my $datasql = "SELECT " . join(',',@datacols) . ' FROM ' . join(' JOIN ', @datatables) . ' WHERE ' . join(' AND ', @dataconditions) . ";";
 
-foreach my $r (@$datarows)
-{
-    my $fileid = $destination . "/" . chomp($$r{'pages.web_id'}) . "/" . $$r{'pages.id'};
-    # encode spaces as well?
-    my $safename = $$r{'pages.name'};
-    $safename =~ s(/)(%23); # check code for /
-    my $filename = $destination . "/" . chomp($$r{'pages.web_id'}) . "/" . $safename;
+
+my $data = $dbh->prepare($datasql);
+$data->execute();
+
+while (my $r = $data->fetchrow_hashref()) {
+    my $fileid = $destination . "/" . $r->{'web_id'} . "/" . $r->{'page_id'};
+    my $safename = uri_escape($r->{'name'});
+    my $filename = $destination . "/" . $r->{'web_id'} . "/" . $safename;
     my $action = (-r $filename ? ' edited ' : ' created ');
     print "Would now write to " . $fileid . "\n";
     print "And link it to     " . $safename . "\n";
-    print "Then commit with   " . $$r{'revisions.author'} . $action . $$r{'pages.name'} . ' on ' . $$r{'revisions.updated_at'} . ' from ' . $$r{'revisions.ip'} . "\n";
+    print "Then commit with   " . $r->{'author'} . $action . $r->{'name'} . ' on ' . $r->{'updated_at'} . ' from ' . $r->{'ip'} . "\n";
 #    open(FILE,">" . $fileid)
 #	or die;
 #    print FILE $r{'revisions.content'};
 #    close(FILE);
 #    link($fileid,$filename);
 #    bzr_commit($r{'revisions.author'} . $action . $r{'pages.name'} . ' on ' . $r{'revisions.updated_at'} . ' from ' . $r{'revisions.ip'});
-    $lastID = $$r{'revisions.id'} if ($$r{'revisions.id'} > $lastID);
+    $lastID = $r->{'id'} if ($r->{'id'} > $lastID);
 }
 
-print "Last identifier was " . $lastID . "\n";
-
-sub getRows
-{
-    my ($db,$cols,$tabs,$conds) = @_;
-
-    my $sql = "'SELECT " . join(',',@$cols) . ' FROM ' . join(' JOIN ', @$tabs) . ($#$conds > 0 ? ' WHERE ' . join(' AND ', @$conds): '') . ";'";
-    my @rows;
-    my @r;
-    my $sep = "SEP".$$;
-
-    my @data = qx(sqlite3 -separator $sep $db $sql);
-
-    my %row = ();
-    my $col;
-
-    while ($_ = shift @data)
-    {
-	if (/^\d+$sep/)	{
-	    if (%row) {
-		chomp($row{$col});
-		push (@rows,\%row);
-		%row = ();
-		push(@$cols,shift(@$cols));
-	    }
-	}
-	my @line = split($sep, $_);
-	$col = $$cols[0];
-	$row{$col} .= shift(@line);
-
-	while (@line) {
-	    push(@$cols,shift(@$cols));
-	    $col = $$cols[0];
-	    $row{$col} .= shift(@line);
-	}
-    }
-
-    return \@rows;
+if (open (CONFIG, ">$config")) {
+    print CONFIG "LastRevision = " . $lastID . "\n";
 }
